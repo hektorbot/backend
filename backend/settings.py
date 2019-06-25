08f2040356edb132a5441713fb6041107071e2e2
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-06-25 15:20:56 -0400

    feat: Enable CORS

--- backend/settings.py
+++ backend/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = ["127.0.0.1", os.getenv("APP_HOST")]
 INSTALLED_APPS = [
     "image_processor.apps.ImageProcessorConfig",
     "rest_framework",
+    "corsheaders",
     "django_q",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -64,6 +66,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "backend.urls"
 
